23a1f0d2fa09e99691d5fe0e47b24541c2e3aa55
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-11-17 17:47:48 +0530

    Task2 completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain="dev-nr32vw4h64b4ijip.us.auth0.com"
+      clientId="rdqiLKJaabD0e1qAftIg9jaiqFptP5AU"
+      redirectUri={"http://localhost:3000"}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
